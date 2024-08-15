73b88a451e609ecfe14a475257f360312fa377a9
Dumb-Tree <ilikemyflamingo@gmail.com>
2024-08-15 16:02:50 +0700

    update login
    login transfer data to main

---
+++ HTML_BTL/JS_BTL/JS_Login.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    const form = document.getElementById('loginInfo');
+    const Username = document.getElementById('Username');
+    const Password = document.getElementById('Password');
+    form.addEventListener("submit",function(e){
+        e.preventDefault();
+
+        const UsernameValue = Username.value;
+        const PasswordValue = Password.value;
+        localStorage.setItem('Username',UsernameValue);
+        localStorage.setItem('Password',PasswordValue);
+        window.location.href="test4.html";
+    })
+}
