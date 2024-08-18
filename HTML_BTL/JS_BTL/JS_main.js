b1b488f059bd096105da19223e616bf3d54fa0d2
NhaanThieen <nguyenvuthiennhan05@gmail.com>
2024-08-18 23:26:15 +0700

    banner v1

---
+++ HTML_BTL/JS_BTL/JS_main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    var bannerlist = document.querySelector('.bannerlist')
+    var img = document.querySelectorAll('.bannerlist img')
+    var slAnh = img.length
+    img = img[0].offsetWidth;
+    var current = 0
+    setInterval(function () {
+        current = current + img * -1;
+        if (current <= -(img * slAnh)) {
+            current = 0;
+        }
+        bannerlist.style.transform = `translateX(${current}px)`
+    }, 1000)
+}
