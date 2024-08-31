1c711345acb65cffd7c8fd736a3fefcb5ef3dbc4
Dumb-Tree <ilikemyflamingo@gmail.com>
2024-08-31 15:13:37 +0700

    update main

--- HTML_BTL/JS_BTL/JS_main.js
+++ HTML_BTL/JS_BTL/JS_main.js
@@ -31,4 +31,25 @@ window.onload = function () {
         else
         d.style.top = count*8.79+8.9+'rem';
     },3999);
+    /*JS cho game list*/
+    let slideIndex = [1,1];
+    let slideId = ["gameSlides1", "gameSlides2"]
+    showSlides(1, 0);
+    showSlides(1, 1);
+    
+    function plusSlides(n, no) {
+      showSlides(slideIndex[no] += n, no);
+    }
+    
+    function showSlides(n, no) {
+      let i;
+      let x = document.getElementsByClassName(slideId[no]);
+      if (n > x.length) {slideIndex[no] = 1}    
+      if (n < 1) {slideIndex[no] = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";  
+      }
+      x[slideIndex[no]-1].style.display = "block";  
+    }
+    addEventListener()
 }
