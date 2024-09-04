ae0804485ee8f192504fa1a3879deaf6271d403d
Dumb-Tree <ilikemyflamingo@gmail.com>
2024-09-04 20:25:22 +0700

    Delete DONE_JS_4.js

--- HTML_BTL/JS_BTL/DONE_JS_4.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    const Username = localStorage.getItem('Username')
-    const Password = localStorage.getItem('Password')
-    //dùng textcontent để hiện mọi nội dung chữ bao gồm cả chữ ẩn
-    document.getElementById('Username').textContent  = Username;
-    document.getElementById('Password').textContent = Password;
-}
